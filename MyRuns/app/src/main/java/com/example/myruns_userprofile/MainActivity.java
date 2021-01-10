bda0b29ff3c4ed9de6b2a4a989fd742a9ecb73f0
Grace Wang <gracewanggw1011@gmail.com>
2021-01-10 11:10:53 -0700

    MyRuns_UserProfile

---
+++ MyRuns/app/src/main/java/com/example/myruns_userprofile/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myruns_userprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeProfile(View view){
+
+    }
+}
